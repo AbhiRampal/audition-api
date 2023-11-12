311ea7a3b202f7446c33728d3240ad56ab3ad326
Abhi <rampal.abhi@gmail.com>
2023-11-13 10:51:13 +1300

    Added @NotNull annotation for API request parameters and @JsonInclude(Include.NON_NULL) to remove NULL objects from JSON response.

--- src/main/java/com/audition/web/AuditionController.java
+++ src/main/java/com/audition/web/AuditionController.java
@@ -3,14 +3,14 @@ package com.audition.web;
 import com.audition.model.AuditionPost;
 import com.audition.service.AuditionService;
 import com.audition.validation.NumericString;
+import jakarta.validation.constraints.NotNull;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.util.MultiValueMap;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,43 +19,34 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AuditionController {
 
+    private final AuditionService auditionService;
+
     @Autowired
-    AuditionService auditionService;
+    public AuditionController(AuditionService auditionService) {
+        this.auditionService = auditionService;
+    }
 
-    // TODO Add a query param that allows data filtering. The intent of the filter is at developers discretion.
-    @RequestMapping(value = "/posts", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public @ResponseBody List<AuditionPost> getPosts(@RequestParam MultiValueMap<String, String> filters) {
-
-        // TODO Add logic that filters response data based on the query param
-
+    @GetMapping(value = "/posts", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody List<AuditionPost> getPosts(
+        @RequestParam(required = false) MultiValueMap<String, String> filters) {
         return auditionService.getPosts(filters);
     }
 
-    @RequestMapping(value = "/posts/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public @ResponseBody AuditionPost getPosts(@PathVariable("id") @NumericString final String postId) {
-        final AuditionPost auditionPosts = auditionService.getPostById(postId);
-
-        // TODO Add input validation
-
-        return auditionPosts;
+    @GetMapping(value = "/posts/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody AuditionPost getPosts(@PathVariable("id") @NumericString @NotNull final String postId) {
+        return auditionService.getPostById(postId);
     }
 
-    @RequestMapping(value = "/posts/{postId}/comments", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public @ResponseBody AuditionPost getPostWithComments(@NumericString @PathVariable("postId") String postId) {
-        final AuditionPost auditionPosts = auditionService.getPostWithComments(postId);
-        // TODO Add input validation
-
-        return auditionPosts;
+    @GetMapping(value = "/posts/{postId}/comments", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody AuditionPost getPostWithComments(
+        @PathVariable("postId") @NumericString @NotNull final String postId) {
+        return auditionService.getPostWithComments(postId);
     }
 
-    // TODO Add additional methods to return comments for each post. Hint: Check https://jsonplaceholder.typicode.com/
-
-    @RequestMapping(value = "/comments", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public @ResponseBody AuditionPost getPostWithCommentsPathVar(@NumericString @RequestParam("postId") String postId) {
-        final AuditionPost auditionPosts = auditionService.getPostWithCommentsPathVar(postId);
-        // TODO Add input validation
-
-        return auditionPosts;
+    @GetMapping(value = "/comments", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody AuditionPost getPostWithCommentsPathVar(
+        @RequestParam("postId") @NumericString @NotNull final String postId) {
+        return auditionService.getPostWithCommentsPathVar(postId);
     }
-
 }
+
